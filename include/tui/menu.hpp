08628fe656aa18961131a55eefcf4ece7c559a29
doomed-phobos <the_ekj.2121@outlook.com>
2023-03-05 15:36:54 -0500

    Simple menu

--- include/tui/menu.hpp
+++ include/tui/menu.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "tui/point.hpp"
 #include "tui/text_format.hpp"
+#include "tui/key.hpp"
 
 #include <string>
 #include <functional>
@@ -8,38 +9,41 @@
 
 namespace tui {
    class System;
-   class Event;
 
    class Menu {
    public:
       class Option {
       public:
-         typedef std::function<void()> Listener;
+         typedef std::function<void(Option&)> Listener;
 
          Option(const char* label) :
             m_label{label},
             m_listener{nullptr},
-            m_focused{false},
-            m_enabled{true} {}
+            m_focused{false} {}
          
-         void setOnListener(Listener&& l) {m_listener = std::move(l);}
-         void enable(bool b) {m_enabled = b;}
-         void focus(bool b) {if(isEnabled()) m_focused = b;}
+         Option& setOnListener(Listener&& l) {m_listener = std::move(l); return *this;}
          bool isFocused() const {return m_focused;}
-         void select() {if(isEnabled() && m_listener) m_listener();}
+         void select() {if(m_listener) m_listener(*this);}
          
          const std::string& label() const {return m_label;}
-         bool isEnabled() const {return m_enabled;}
       private:
+         friend class Menu;
+         void focus(bool b) {m_focused = b;}
+         
          std::string m_label;
          Listener m_listener;
-         bool m_enabled;
          bool m_focused;
       };
 
-      Menu(TextColor text_color = s_default_text_color, TextColor pointer_color = s_default_pointer_color);
+      Menu() :
+         m_text_color{s_default_text_color},
+         m_pointer_color{s_default_pointer_color},
+         m_offset{s_default_offset},
+         m_current_option{nullptr} {}
 
       void setOffset(ushort offset) {m_offset = offset;}
+      void setTextColor(const TextFormat& color) {m_text_color = color;}
+      void setPointerColor(const TextFormat& color) {m_pointer_color = color;}
 
       Option& addOption(const char* label) {
          m_options.emplace_back(label);
@@ -47,17 +51,19 @@ namespace tui {
       }
       bool hasOptions() const {return !m_options.empty();}
 
-      void event(const Event& ev);
-      void draw(const System* sys);
+      void update();
    private:
       static constexpr inline const ushort s_default_offset = 3;
-      static constexpr inline const TextColor s_default_text_color = kDefault_TextColor;
-      static constexpr inline const TextColor s_default_pointer_color = kYellow_TextColor;
+      static constexpr inline const TextFormat& s_default_text_color = formats::kDefault_TextColor;
+      static constexpr inline const TextFormat& s_default_pointer_color = formats::kYellow_TextColor;
       void findCurrentOption();
-      void drawOption(const Option& o, const System* sys);
+      void drawOption(const Option& o);
+      
+      void event();
+      void draw();
 
-      TextColor m_text_color;
-      TextColor m_pointer_color;
+      TextFormat m_text_color;
+      TextFormat m_pointer_color;
       Option* m_current_option;
       ushort m_offset {s_default_offset};
       std::vector<Option> m_options;
--- src/menu.cc
+++ src/menu.cc
@@ -1,77 +1,63 @@
 #include "tui/menu.hpp"
 
 #include "tui/system.hpp"
-#include <algorithm>
 
 namespace tui {
-   Menu::Menu(TextColor text_color, TextColor pointer_color) :
-      m_text_color{text_color},
-      m_pointer_color{pointer_color},
-      m_current_option{nullptr} {} 
-
-   void Menu::event(const Event& ev) {
-      if(!hasOptions() || !m_current_option)
+   void Menu::update() {
+      if(!hasOptions())
          return;
-      
-      switch(ev.keycode) {
+
+      draw();
+      instance->forcePaint();
+      event();
+   }
+
+   void Menu::draw() {
+      findCurrentOption();
+
+      for(const auto& option : m_options)
+         drawOption(option);
+   }
+
+   void Menu::findCurrentOption() {
+      if(!m_current_option) {
+         m_current_option = &m_options.front();
+         m_current_option->focus(true);
+      }
+   }
+
+   void Menu::drawOption(const Option& o) {
+      auto xy = instance->getXY();
+      instance->moveTo(xy + Point{s_default_offset, 0});
+      instance->put('[', m_text_color);
+      instance->put((o.isFocused() ? '+' : ' '), m_pointer_color | formats::kBold_TextStyle);
+      instance->put(']', m_text_color);
+      instance->printf(m_text_color, " %s\n", o.label().c_str());
+   }
+
+   void Menu::event() {
+      switch(instance->waitKeyDown()) {
          case kDownArrow_KeyCode: {
-         // FIXME: Possible bug?
-            for(auto it = std::vector<Option>::iterator(m_current_option); ++it < m_options.end();) {
-               if(it->isEnabled()) {
-                  m_current_option->focus(false);
-                  m_current_option = &(*it);
-                  m_current_option->focus(true);
-                  break;
-               }
+            auto it = std::next(std::vector<Option>::iterator(m_current_option));
+            if(it != m_options.end()) {
+               m_current_option->focus(false);
+               m_current_option = &*it;
+               m_current_option->focus(true);
             }
          }
-            break;
+         break;
          case kUpArrow_KeyCode: {
-         // FIXME: Possible bug?
-            for(auto it = std::vector<Option>::iterator(m_current_option); --it >= m_options.begin();) {
-               if(it->isEnabled()) {
-                  m_current_option->focus(false);
-                  m_current_option = &(*it);
-                  m_current_option->focus(true);
-                  break;
-               }
+            auto it = std::next(std::vector<Option>::iterator(m_current_option), -1);
+            if(it >= m_options.begin()) {
+               m_current_option->focus(false);
+               m_current_option = &*it;
+               m_current_option->focus(true);
             }
-            break;
          }
+         break;
          case kEnter_KeyCode:
             m_current_option->select();
             break;
       }
-   }
-
-   void Menu::draw(const System* sys) {
-      findCurrentOption();
-      
-      for(const auto& option : m_options)
-         drawOption(option, sys);
-   }
-
-   void Menu::findCurrentOption() {
-      if(m_current_option)
-         return;
-
-      for(auto& option : m_options) {
-         if(option.isEnabled()) {
-            m_current_option = &option;
-            m_current_option->focus(true);
-            return;
-         }
-      }
-      m_current_option = m_options.data() + m_options.size();
-   }
-
-   void Menu::drawOption(const Option& o, const System* sys) {
-      auto xy = sys->getXY();
-      TextFormat text_format{m_text_color, (o.isEnabled() ? kDefault_TextStyle : kDim_TextStyle)};
-      sys->moveTo(xy + Point{s_default_offset, 0});
-      sys->put('[', text_format);
-      sys->put((o.isFocused() ? '*' : ' '), {m_pointer_color, kBold_TextStyle});
-      sys->put(']', text_format);
-      sys->printf(text_format, " %s\n", o.label().c_str());
    }
 } // namespace tui
