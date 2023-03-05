1bdfcc98268cf9c6528c2f7c6e3c90d2f379ed15
doomed-phobos <the_ekj.2121@outlook.com>
2023-03-05 15:34:59 -0500

    Changed TextFormat class
    * Added operator | to text styles

--- include/tui/text_format.hpp
+++ include/tui/text_format.hpp
@@ -1,43 +1,66 @@
 #pragma once
+#include <cstdint>
 
 namespace tui {
-   enum TextColor : short {
-      kDefault_TextColor       = 0x00,
-      kRed_TextColor           = 0x01,
-      kGreen_TextColor         = 0x02,
-      kYellow_TextColor        = 0x03,
-      kBlue_TextColor          = 0x04,
-      kMagenta_TextColor       = 0x05,
-      kCyan_TextColor          = 0x06,
-      kWhite_TextColor         = 0x07,
-      kGray_TextColor          = 0x08,
-      kLight_Red_TextColor     = 0x09,
-      kLight_Green_TextColor   = 0x0a,
-      kLight_Yellow_TextColor  = 0x0b,
-      kLight_Blue_TextColor    = 0x0c,
-      kLight_Magenta_TextColor = 0x0d,
-      kLight_Cyan_TextColor    = 0x0e,
-      kBright_White_TextColor  = 0x0f,
-
-      kLastColor_TextColor     = kBright_White_TextColor,
-   };
-
-   enum TextStyle {
-      kDefault_TextStyle = 0,
-      kBold_TextStyle    = 1 << 1,
-      kDim_TextStyle     = 1 << 2, // Half bright
-   };  
-
    class TextFormat {
    public:
-      TextFormat();
-      TextFormat(TextColor color);
-      TextFormat(TextColor color, unsigned style);
+      uint8_t color() const {return (uint8_t) ((m_value >> 28) & 0xff);}
+      uint32_t style() const {return (uint32_t) (m_value & 0xfffffff);}
 
-      unsigned int ncurses_style() const {return m_style;}
-      short ncurses_color() const {return m_color;}
+      TextFormat operator|(const uint32_t style) const {
+         return TextFormat(m_value | style);
+      }
+
+      static TextFormat MakeFromColor(uint8_t color) {
+         return TextFormat((uint64_t)color << 28);
+      }
    private:
-      unsigned m_style;
-      short m_color;
+      TextFormat(uint64_t value) :
+         m_value{value} {}
+
+      uint64_t m_value;
    };
+
+   namespace formats {
+      const TextFormat kDefault_TextColor       = TextFormat::MakeFromColor(0x00); 
+      const TextFormat kRed_TextColor           = TextFormat::MakeFromColor(0x01);
+      const TextFormat kGreen_TextColor         = TextFormat::MakeFromColor(0x02);
+      const TextFormat kYellow_TextColor        = TextFormat::MakeFromColor(0x03);
+      const TextFormat kBlue_TextColor          = TextFormat::MakeFromColor(0x04);
+      const TextFormat kMagenta_TextColor       = TextFormat::MakeFromColor(0x05);
+      const TextFormat kCyan_TextColor          = TextFormat::MakeFromColor(0x06);
+      const TextFormat kWhite_TextColor         = TextFormat::MakeFromColor(0x07);
+      const TextFormat kGray_TextColor          = TextFormat::MakeFromColor(0x08);
+      const TextFormat kLight_Red_TextColor     = TextFormat::MakeFromColor(0x09);
+      const TextFormat kLight_Green_TextColor   = TextFormat::MakeFromColor(0x0a);
+      const TextFormat kLight_Yellow_TextColor  = TextFormat::MakeFromColor(0x0b);
+      const TextFormat kLight_Blue_TextColor    = TextFormat::MakeFromColor(0x0c);
+      const TextFormat kLight_Magenta_TextColor = TextFormat::MakeFromColor(0x0d);
+      const TextFormat kLight_Cyan_TextColor    = TextFormat::MakeFromColor(0x0e);
+      const TextFormat kBright_White_TextColor  = TextFormat::MakeFromColor(0x0f);
+
+      extern const uint32_t kBold_TextStyle;
+      extern const uint32_t kDim_TextStyle;
+
+      const TextFormat colors[] = {
+         kDefault_TextColor,
+         kRed_TextColor,
+         kGreen_TextColor,
+         kYellow_TextColor,
+         kBlue_TextColor,
+         kBlue_TextColor,
+         kMagenta_TextColor,
+         kCyan_TextColor,
+         kWhite_TextColor,
+         kGray_TextColor,
+         kLight_Red_TextColor,
+         kLight_Green_TextColor,
+         kLight_Yellow_TextColor,
+         kLight_Magenta_TextColor,
+         kLight_Blue_TextColor,
+         kLight_Magenta_TextColor,
+         kLight_Cyan_TextColor,
+         kBright_White_TextColor,
+      };
+   } // namespace formats
 } // namespace tui
--- src/text_format.cc
+++ src/text_format.cc
@@ -2,37 +2,7 @@
 
 #include <ncurses.h>
 
-namespace tui {
-   /*
-   FIXME:
-      For me, who use a few of menus this function isn't causing me any performance problem.
-
-      Performance pseudo-analysis in 5 options:
-         *) Without: 63.6 ms
-         *) With:    89 ms
-      
-      Maybe to define TextStyle in compilation-time?
-      Maybe to use a tui thread?
-   */
-   static constexpr inline unsigned convert_from_ncurses(int style) {
-      int ncurses_style = 0;
-
-      if(style != kDefault_TextStyle) {
-         if(style & kBold_TextStyle)
-            ncurses_style |= A_BOLD;
-         if(style & kDim_TextStyle)
-            ncurses_style |= A_DIM;
-      } else {
-         ncurses_style = A_NORMAL;
-      }
-      
-      return ncurses_style;
-   }
-
-   TextFormat::TextFormat() :
-      TextFormat{kDefault_TextColor, kDefault_TextStyle} {}
-   TextFormat::TextFormat(TextColor color) :
-      TextFormat{color, kDefault_TextStyle} {}
-   TextFormat::TextFormat(TextColor color, unsigned style) :
-      m_color{color}, m_style{convert_from_ncurses(style)} {}
-} // namespace tui
+namespace tui::formats {
+   const uint32_t kBold_TextStyle    = A_BOLD;
+   const uint32_t kDim_TextStyle     = A_DIM;
+} // namespace tui::formats
